ce7e52f8f5688dd0e6987027ec87d1530ec5158c
amina-iguider <iguider.aminae@gmail.com>
2024-09-23 17:13:18 +0100

    Correction de barème de jauge

---
+++ mme_bareme/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Barème MME",
+    "version": "16.0.1.0.0",
+    "category": "Quality control",
+    "license": "AGPL-3",
+    "author": "Tecpod & MME",
+    "website": "tecpod.ma",
+    "depends": ["base", "mme_production", "mrp", "product",
+                "stock", "sale_management", "project", "uom"],
+    "data": [
+        "data/product_jauge.xml"
+
+    ],
+    'application': True,
+    "installable": True,
+    "auto_install": True,
+}
